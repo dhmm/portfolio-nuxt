008d1a08944772ad3b337c4043f37c00d9347d5f
= <=>
2024-08-17 16:37:40 +0300

    getting route paramters

---
+++ app/pages/blog/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        We are inside of a blog!
+    </div>
+    <div>
+        The <b>id</b> is : <b><u>{{ route.params.id }}</u></b>
+    </div>
+</template>
+
+<script setup>
+// ## We will use the composable useRoute
+const route = useRoute();
+console.log(route);
+</script>
