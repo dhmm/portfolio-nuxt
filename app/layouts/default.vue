60e6fea978d4499cc40a3d3041e1989926724842
= <=>
2024-08-18 22:41:11 +0300

    some styles added

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>
-        <nav>
-            <ul>
-                <li><NuxtLink to="/">Home page</NuxtLink></li>
-                <li><NuxtLink to="/about">About page</NuxtLink></li>
-            </ul>
-        </nav>
-        <slot/>
+    <div class="container mx-auto max-w-2xl">
+        <header class="flex justify-between items-center mt-5">
+            <div class="text-xl font-semibold p-2 hover:bg-gray-200">
+                <NuxtLink to="/">Moutlou</NuxtLink>
+            </div>
+            <nav class="font-mono">
+                <ul class="flex space-x-4">
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main class="p-2">
+            <slot/>
+        </main>
     </div>
 </template>
